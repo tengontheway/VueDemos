814908736075abd6af8f1a8ff3eed39abc9c9efe
evilcode <teng_ontheway@163.com>
2018-07-05 16:53:45 +0800

    lesson10: v-if、v-show、v-for

---
+++ lesson10/index.js
@@ -0,0 +1,23 @@
+/**
+ * Created by evilcode on 2018/7/4.
+ */
+var app = new Vue({
+    el: '#vue-app',
+    data: {
+        isSucceed: false,
+        isFailed: false,
+
+        names: ['tzt', 'cjp', 'lip'],
+        users: [
+            { name: 'Teng', age: 18 },
+            { name: 'Liu', age: 20 },
+            { name: 'Cai', age: 17 }
+        ]
+    },
+    methods: {
+
+    },
+    computed: {
+
+    }
+})
