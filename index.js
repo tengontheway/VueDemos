12be56e0ff92773ce6bd49bcb1fcd0d726c65708
evilcode <teng_ontheway@163.com>
2018-07-11 10:10:59 +0800

    Lesson 12 多个实例

--- index.js
+++ index.js
@@ -1,27 +1,42 @@
 /**
  * Created by evilcode on 2018/7/4.
  */
-var app = new Vue({
-    el: '#vue-app',
+var app1 = new Vue({
+    el: '#vue-app1',
     data: {
-        health: 100,
-        isOver: false
+        name: 'tzt',
+        age: 18
     },
     methods: {
-        hit: function() {
-            this.health -= 10
-            if (this.health <= 0) {
-                this.health = 0
-                this.isOver = true
-            }
-        },
-
-        restart: function() {
-            this.health = 100
-            this.isOver = false
+        greet: function() {
+            return "hello app 1"
         }
     },
     computed: {
+        calcAge: function() {
+            return  this.age
+        }
+    }
+})
 
+
+var app2 = new Vue({
+    el: '#vue-app2',
+    data: {
+        name: 'li',
+        age: 20,
+    },
+    methods: {
+        greet: function() {
+            return "hello app 2"
+        },
+        changeName: function() {
+            app1.name = 'hello'
+        }
+    },
+    computed: {
+        calcAge: function() {
+            return  this.age
+        }
     }
 })
